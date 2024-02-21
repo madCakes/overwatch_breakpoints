feb0cd8e09a51b20b20c8a0768069b5a5d1e0c0f
madCakes <jason.pbrooks@hotmail.com>
2024-02-21 20:32:47 +0000

    Initial schema

---
+++ heroData.js
@@ -0,0 +1,23 @@
+heroes = {
+    ana: {
+        health: 250,
+        armour: 0,
+        shields: 0,
+        primaryDamage: 70,
+        secondaryDamage: 0,
+        critMultiplier: 2,
+        healing: 70,
+        primaryRateOfFire: 0.8, // in seconds
+        rateOfHealing: 0.8, // in seconds
+        tickValue: 0.59, // in seconds
+        abilities: {
+            abilityName: {
+                dmg: 0,
+                statusEffect: null,
+                duration: 0, // in seconds
+            },
+        },
+    },
+
+    
+}
